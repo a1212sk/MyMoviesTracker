bf73bf33d6314cd269647f3c3cbd25c40fef62d5
Alexander Skornyakov <a1212sk@gmail.com>
2020-08-24 12:40:12 +0300

    Add anonymous authentication

--- app/src/main/java/alexander/skornyakov/mymoviestracker/MainActivity.kt
+++ app/src/main/java/alexander/skornyakov/mymoviestracker/MainActivity.kt
@@ -2,10 +2,42 @@ package alexander.skornyakov.mymoviestracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        auth = Firebase.auth
     }
+
+    override fun onStart() {
+        super.onStart()
+        if(!isUserSignedIn()){
+            signInAnonymously()
+        }
+    }
+
+    private fun isUserSignedIn() : Boolean{
+        return auth?.currentUser != null
+    }
+
+    private fun signInAnonymously(){
+        auth.signInAnonymously().addOnCompleteListener {
+            if(it.isSuccessful){
+                println("Logged in ${auth.currentUser?.uid}")
+            }
+            else{
+                println("Something goes wrong! Quitting...")
+                finish()
+            }
+        }
+    }
+
 }
