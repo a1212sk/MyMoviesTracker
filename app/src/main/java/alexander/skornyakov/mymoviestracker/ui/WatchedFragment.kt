647b9c4c14a5d6d10ca00bdf8935869bbafbbe2a
Alexander Skornyakov <a1212sk@gmail.com>
2020-09-22 16:55:32 +0300

    fix watched button in watched rv item

--- app/src/main/java/alexander/skornyakov/mymoviestracker/ui/WatchedFragment.kt
+++ app/src/main/java/alexander/skornyakov/mymoviestracker/ui/WatchedFragment.kt
@@ -3,27 +3,27 @@ package alexander.skornyakov.mymoviestracker.ui
 import alexander.skornyakov.mymoviestracker.R
 import alexander.skornyakov.mymoviestracker.helpers.hide
 import alexander.skornyakov.mymoviestracker.helpers.show
-import alexander.skornyakov.mymoviestracker.ui.adapters.WatchingMoviesRVAdapter
+import alexander.skornyakov.mymoviestracker.ui.adapters.WatchedMoviesRVAdapter
 import alexander.skornyakov.mymoviestracker.viewmodels.WatchedViewModel
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import dagger.hilt.android.AndroidEntryPoint
-import androidx.lifecycle.Observer
 import kotlinx.android.synthetic.main.fragment_watched.*
 
 @AndroidEntryPoint
-class WatchedFragment : Fragment(R.layout.fragment_watched){
+class WatchedFragment : Fragment(R.layout.fragment_watched) {
 
-    val viewModel : WatchedViewModel by viewModels()
+    val viewModel: WatchedViewModel by viewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         val adapter =
-            WatchingMoviesRVAdapter()
+            WatchedMoviesRVAdapter()
         recyclerView.layoutManager = LinearLayoutManager(requireContext())
         recyclerView.adapter = adapter
 
